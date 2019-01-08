ef8457c2f3ef51cd83b25c05db1b5d883981c272
Your Name <you@example.com>
2019-01-09 03:46:02 +0900

    under line - error 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import PageTemplate from './PageTemplate';
-import TodoInput from './TodoInput/Todoinput';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <PageTemplate><TodoInput/></PageTemplate>
-        
+        <PageTemplate>
+        <TodoInput/>
+        <TodoList/>
+        </PageTemplate>
       </div>
     )
   }
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './TodoItem';
+import './TodoItem.scss';
 
 
 
